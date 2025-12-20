297559ee2c3efcb6d43734ced5d27793dac83f75
Brayan Lopes <brylps90@outlook.com>
2025-12-19 21:21:19 -0300

    Pegando os elementos que irão receber o valor digitado pelo usuário

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Pegando os elementos que irão receber o valor digitado pelo usuário
+const number = document.getElementById("number")
+const numberFrom = document.getElementById("numberFrom")
+const numberTo = document.getElementById("numberTo")
