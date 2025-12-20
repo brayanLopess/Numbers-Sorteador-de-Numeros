54f82f8d9a7e10cbe92c3e8358ec7aa5191cb501
Brayan Lopes <brylps90@outlook.com>
2025-12-19 21:26:10 -0300

    Pegando o valor dos elementos que o usuário digitou

--- scripts.js
+++ scripts.js
@@ -2,3 +2,8 @@
 const number = document.getElementById("number")
 const numberFrom = document.getElementById("numberFrom")
 const numberTo = document.getElementById("numberTo")
+
+// Pegando o valor dos elementos que o usuário digitou
+const numberValue = number.value
+const numberFromValue = numberFrom.value
+const numberToValue = numberTo.value
