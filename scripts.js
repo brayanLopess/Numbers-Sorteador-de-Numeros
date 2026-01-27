c99010a3c04277463e64626cdd36da1bd7790c72
Brayan Lopes <brylps90@outlook.com>
2026-01-27 15:58:46 -0300

    Validando se os campos do input não estão vazios e validando se o máximo é maior que o mínimo para poder ocorrer o sorteio.

--- scripts.js
+++ scripts.js
@@ -1,50 +1,73 @@
 // Inputs de número
-const number = document.getElementById("number")
-const numberFrom = document.getElementById("numberFrom")
-const numberTo = document.getElementById("numberTo")
+const number = document.getElementById("number");
+const numberFrom = document.getElementById("numberFrom");
+const numberTo = document.getElementById("numberTo");
 
 // Botão de checkbox
-const toggleButton = document.getElementById("toggleButton")
+const toggleButton = document.getElementById("toggleButton");
 
 // Botão Sortear
-const raffleNumberButton = document.getElementById("botaoSortear")
+const raffleNumberButton = document.getElementById("botaoSortear");
 
 // Regex
-const hasCharactersRegex = /\D/g
+const hasCharactersRegex = /\D/g;
 
 number.oninput = () => {
-    const numberValue = number.value.replace(hasCharactersRegex, "")
-    number.value = numberValue
-}
+  const numberValue = number.value.replace(hasCharactersRegex, "");
+  number.value = numberValue;
+};
 
 numberFrom.oninput = () => {
-    const numberValue = numberFrom.value.replace(hasCharactersRegex, "")
-    numberFrom.value = numberValue
-}
+  const numberValue = numberFrom.value.replace(hasCharactersRegex, "");
+  numberFrom.value = numberValue;
+};
 
 numberTo.oninput = () => {
-    const numberValue = numberTo.value.replace(hasCharactersRegex, "")
-    numberTo.value = numberValue
-}
+  const numberValue = numberTo.value.replace(hasCharactersRegex, "");
+  numberTo.value = numberValue;
+};
 
 function toRaffle() {
-    let numberValue = Number(number.value)
-    let numberFromValue = Number(numberFrom.value)
-    let numberToValue = Number(numberTo.value)
+  let numberValue = Number(number.value);
+  let numberFromValue = Number(numberFrom.value);
+  let numberToValue = Number(numberTo.value);
 
-    let results = []
-    for(numberCount = 0; numberCount < numberValue; numberCount++) {
-        let numRaffled = Math.floor(Math.random() * (numberToValue - numberFromValue) + numberFromValue)
-        let valueOfNum = Number(numRaffled.valueOf())
-        if(!results.includes(numRaffled)) {
-            results.push(numRaffled.valueOf())
+  let results = [];
+  if (numberValue && numberFromValue && numberToValue !== null) {
+    if (numberToValue > numberFromValue) {
+      for (numberCount = 0; numberCount < numberValue; numberCount++) {
+        let numRaffled = Math.floor(
+          Math.random() * (numberToValue - numberFromValue) + numberFromValue,
+        );
+        let valueOfNum = Number(numRaffled.valueOf());
+        if (!results.includes(numRaffled)) {
+          results.push(numRaffled.valueOf());
         } else {
-            results.push(Math.floor(Math.random() * (numberToValue - numberFromValue !== valueOfNum) + numberFromValue + valueOfNum))
+          results.push(
+            Math.floor(
+              Math.random() * (numberToValue - numberFromValue !== valueOfNum) +
+                numberFromValue +
+                valueOfNum,
+            ),
+          );
         }
+      }
+    } else {
+         alert(
+      "Não foi possível realizar o sorteio pois o máximo NÃO é maior que o mínimo. Por favor, insira um número máximo maior que o mínimo e tente novamente.",
+    );
     }
-    console.log(results)
+  } else {
+    alert("Não foi possível realizar o sorteio pois não tem nenhum valor ou está faltando colocar um número. Por favor, insira os números corretamente e tente novamente.")
+  }
+  console.log(results);
 }
 
 raffleNumberButton.onclick = () => {
-    toRaffle()
-}
+  toRaffle();
+
+  // Limpando os números anteriores antes de cada sorteio.
+  number.value = ""
+  numberTo.value = ""
+  numberFrom.value = ""
+};
